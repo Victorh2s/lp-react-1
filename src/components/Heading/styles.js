ef4928c56279f8927aa0b5598c9e90b23ad9a424
Victor Henrique <92759987+Victorhdss6@users.noreply.github.com>
2022-05-24 12:04:50 -0300

    Test component (Heading)

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -19,7 +19,6 @@ const titleSize = {
 const mediaFont = (theme) => css`
   @media ${theme.media.lteMedium} {
     font-size: ${theme.font.sizes.xlarge};
-    background: purple;
   }
 `;
 
--- src/templates/Home/Home.test.js
+++ src/templates/Home/Home.test.js
@@ -4,17 +4,9 @@ import { theme } from '../../styles/theme';
 import Home from './index';
 
 //Renderizando o componente e testando o css
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  debug();
-
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Hello',
-  }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.secondary,
+describe('<Home/>', () => {
+  test('renders learn react link', () => {
+    renderTheme(<Home />);
   });
-
-  expect(headingContainer).toMatchSnapshot();
 });
 // expect(headingContainer).toHaveStyleRule('background', 'blue');
