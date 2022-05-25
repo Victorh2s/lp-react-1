657df20e74c47b60fd6c056bc5ccbebdb76cd195
Victor Henrique <92759987+Victorh2s@users.noreply.github.com>
2022-05-25 13:19:01 -0300

    Test Component (LogoLink)

--- src/components/LogoLink/LogoLink.test.js
+++ src/components/LogoLink/LogoLink.test.js
@@ -1,10 +1,30 @@
 import { screen } from '@testing-library/react';
 import { LogoLink } from '.';
 import { renderTheme } from '../../styles/render-theme';
-describe('<LogoLink/>', () => {
-  it('should render', () => {
-    renderTheme(<LogoLink>Children</LogoLink>);
-    const logolink = screen.getByRole('heading');
-    expect(logolink).toBeInTheDocument();
+
+describe('<LogoLink />', () => {
+  it('should render text logo', () => {
+    renderTheme(<LogoLink link="#target" text="Olá mundo" />);
+    const heading = screen.getByRole('heading', { name: 'Olá mundo' });
+    expect(heading).toBeInTheDocument();
+    const link = screen.getByRole('link', { name: 'Olá mundo' });
+    expect(link).toHaveAttribute('href', '#target');
+  });
+
+  it('should render image logo', () => {
+    renderTheme(
+      <LogoLink link="#target" text="Olá mundo" srcImg="image.jpg" />,
+    );
+    expect(screen.getByAltText('Olá mundo')).toHaveAttribute(
+      'src',
+      'image.jpg',
+    );
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(
+      <LogoLink link="#target" text="Olá mundo" srcImg="image.jpg" />,
+    );
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
