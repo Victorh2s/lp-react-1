b96a1b4e4821fb285f9c6f87509d5d00e6a360e1
Victor Henrique <92759987+Victorh2s@users.noreply.github.com>
2022-05-25 08:47:17 -0300

    Test Component (MenuLink)

--- src/components/MenuLink/MenuLink.test.js
+++ src/components/MenuLink/MenuLink.test.js
@@ -2,9 +2,40 @@ import { screen } from '@testing-library/react';
 import { MenuLink } from '.';
 import { renderTheme } from '../../styles/render-theme';
 describe('<MenuLink/>', () => {
-  it('should render', () => {
-    renderTheme(<MenuLink>Children</MenuLink>);
-    const MenuLink = screen.getByRole('heading');
-    expect(MenuLink).toBeInTheDocument();
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http://localhost">Children</MenuLink>);
+    const menulink = screen.getByRole('link', { name: 'Children' });
+    expect(menulink).toHaveAttribute('target', '_self');
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={true}>
+        Children
+      </MenuLink>,
+    );
+    const menulink = screen.getByRole('link', { name: 'Children' });
+
+    expect(menulink).toHaveAttribute('target', '_blank');
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={false}>
+        Children
+      </MenuLink>,
+    );
+
+    const menulink = screen.getByRole('link', { name: 'Children' });
+
+    expect(menulink).toMatchInlineSnapshot(`
+      <a
+        class="sc-bczRLJ gdZAOM"
+        href="http://localhost"
+        target="_self"
+      >
+        Children
+      </a>
+    `);
   });
 });
