e31dfc1315675b26f1794b6c27d60163a614bee7
Victor Henrique <92759987+Victorh2s@users.noreply.github.com>
2022-05-24 21:20:05 -0300

    Test Component (Text)

--- src/components/TextComponent/Text.test.js
+++ src/components/TextComponent/Text.test.js
@@ -1,12 +1,27 @@
+import 'jest-styled-components';
 import { screen } from '@testing-library/react';
 import { TextComponent } from '.';
 import { renderTheme } from '../../styles/render-theme';
-import 'jest-styled-components';
 
 describe('<TextComponent/>', () => {
   it('should render', () => {
     renderTheme(<TextComponent>Children</TextComponent>);
-    const textcomponent = screen.getByRole('heading');
+    const textcomponent = screen.getByText('Children');
     expect(textcomponent).toBeInTheDocument();
+  });
+
+  it('should render correct text sizes', () => {
+    const { container } = renderTheme(<TextComponent>Children</TextComponent>);
+    expect(container.firstChild).toMatchInlineSnapshot(`
+    .c0 {
+      font-size: 2.4rem;
+    }
+
+    <p
+      class="c0"
+    >
+      Children
+    </p>
+    `);
   });
 });
