9c9f239cb7b066c20e4cbad6e3ec7d28b497398f
Victor Henrique <92759987+Victorh2s@users.noreply.github.com>
2022-05-25 11:43:10 -0300

    Test Component (NavLinks)

--- src/components/NavLinks/NavLinks.test.js
+++ src/components/NavLinks/NavLinks.test.js
@@ -1,10 +1,33 @@
 import { screen } from '@testing-library/react';
 import { NavLinks } from '.';
 import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
+import mock from './mock';
+import 'jest-styled-components';
+
 describe('<NavLinks/>', () => {
   it('should render', () => {
-    renderTheme(<NavLinks>Children</NavLinks>);
-    const NavLinks = screen.getByRole('heading');
-    expect(NavLinks).toBeInTheDocument();
+    renderTheme(<NavLinks links={mock} />);
+    const navlinks = screen.getAllByRole('link');
+    expect(navlinks).toHaveLength(mock.length);
+  });
+
+  it('should not render links', () => {
+    renderTheme(<NavLinks links={mock} />);
+    const navlinks = screen.queryAllByText(/links/i);
+    expect(navlinks).toHaveLength(0);
+  });
+
+  it('should render links', () => {
+    renderTheme(<NavLinks links={mock} />);
+    const navlinks = screen.getByText(/link 10/).parentElement;
+    expect(navlinks).toHaveStyleRule('flex-flow', 'column wrap', {
+      media: theme.media.lteMedium,
+    });
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<NavLinks links={mock} />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
